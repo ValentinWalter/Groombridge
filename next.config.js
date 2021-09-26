3135ef721746e19687f345b9c29de4b8e80f4055
Valentin Walter <valentinwalter@icloud.com>
2021-09-26 18:01:31 +0200

    Add bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 const isProduction = process.env.NODE_ENV === "production"
 
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
+})
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   basePath: isProduction ? "/groombridge" : undefined,
   assetPrefix: isProduction ? "/groombridge/" : undefined,
-}
+})
