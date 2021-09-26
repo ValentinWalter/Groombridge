0ab1e03aca4bec8fca0d2235f8c51706b280f47f
Valentin Walter <valentinwalter@icloud.com>
2021-09-26 19:21:51 +0200

    Change API token and add to .env file

--- model/storage.tsx
+++ model/storage.tsx
@@ -1,8 +1,7 @@
 import { Web3Storage } from "web3.storage"
 
 const storage = new Web3Storage({
-  token:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDhCYTg2OTg2RDcyNDM3MDJiOGNlNUE2RmRlMDgzOEYzODJBNTYwM2QiLCJpc3MiOiJ3ZWIzLXN0b3JhZ2UiLCJpYXQiOjE2MzIzMDkyODYwNTUsIm5hbWUiOiJHcm9vbWJyaWRnZSJ9.V__oHO_nj3W84mGG1S-OYOtlE_NBTtHfyXhRkawJpww",
+  token: process.env.NEXT_PUBLIC_WEB3STORAGE_API_TOKEN ?? "",
 })
 
 export default storage
