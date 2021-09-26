cf43b063f00999c82fce556e7604f90e2491ec06
Valentin Walter <valentinwalter@icloud.com>
2021-09-26 11:38:32 +0200

    Set basePath and assetPrefix for production

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
+const isProduction = process.env.NODE_ENV === "production"
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  basePath: isProduction ? "/groombridge" : undefined,
+  assetPrefix: isProduction ? "/groombridge/" : undefined,
 }
